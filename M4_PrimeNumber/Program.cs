383c66af49a59c53c5d6a94adcda29d2bb44119a
a615125340 <a615125340@yahoo.com.tw>
2018-07-31 16:06:54 +0800

    M4

---
+++ M4_PrimeNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 100; i++)// 被除數
+        {
+            for (int k = 2; k <= i; k++)// 除數
+            {
+                if (i % k == 0 && i != k) // 排除所有在 i=k 之前 能被k整除(餘數為0)的整數
+                {
+                    break;
+                }
+                if (i % k == 0 && i == k) // 輸出所有在 i=k 且 i%k=0的數
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
---
+++ M4_Sum1To100/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+        int i = 1;
+        int x = 0;
+        while (i <= 100) 
+        {
+            x = x + i;
+            i++;
+        }
+        Console.WriteLine(x);
+    }
+}
+
